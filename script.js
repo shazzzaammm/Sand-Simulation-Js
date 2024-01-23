4f00f6872ade1dc893e24cb7919d4407fa5dab36
K <redknclarke@gmail.com>
2024-01-22 20:59:20 -0500

    added pretty colors

--- script.js
+++ script.js
@@ -7,11 +7,49 @@ const rows = Math.floor(height / w);
 const cols = Math.floor(width / w);
 let grid = createGrid();
 let dragging = false;
+let hue = 0;
 function setup() {
   document.body.appendChild(canvas);
   canvas.width = width;
   canvas.height = height;
   setInterval(draw, 30);
+}
+
+function HSVtoRGB(h, s, v) {
+  var r, g, b, i, f, p, q, t;
+  if (arguments.length === 1) {
+    (s = h.s), (v = h.v), (h = h.h);
+  }
+  i = Math.floor(h * 6);
+  f = h * 6 - i;
+  p = v * (1 - s);
+  q = v * (1 - f * s);
+  t = v * (1 - (1 - f) * s);
+  switch (i % 6) {
+    case 0:
+      (r = v), (g = t), (b = p);
+      break;
+    case 1:
+      (r = q), (g = v), (b = p);
+      break;
+    case 2:
+      (r = p), (g = v), (b = t);
+      break;
+    case 3:
+      (r = p), (g = q), (b = v);
+      break;
+    case 4:
+      (r = t), (g = p), (b = v);
+      break;
+    case 5:
+      (r = v), (g = p), (b = q);
+      break;
+  }
+  return {
+    r: Math.round(r * 255),
+    g: Math.round(g * 255),
+    b: Math.round(b * 255),
+  };
 }
 
 function createGrid() {
@@ -32,7 +70,8 @@ function draw() {
     for (let j = 0; j < rows; j++) {
       val = grid[i][j];
       if (val > 0) {
-        fill("white");
+        color = HSVtoRGB(val, 1, 1);
+        fill(`rgb(${color.r},${color.g},${color.b})`);
         rect(i * w, j * w, w, w);
 
         let left, right;
@@ -79,7 +118,9 @@ function mouseDragged(e) {
   if (!dragging) return;
   x = Math.floor(e.pageX / w);
   y = Math.floor(e.pageY / w);
-  grid[x][y] = 1;
+  grid[x][y] = hue;
+  hue += 0.0005;
+  if (hue >= 1) hue = 0;
 }
 
 setup();
